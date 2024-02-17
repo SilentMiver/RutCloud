8a1d79d1bf2d12a28b99c2208ab0be07945263a6
SexyBoy12 <russianthief3@gmail.com>
2024-02-17 17:00:19 +0300

    Initial commit

---
+++ src/main/java/com/example/rutcloud/RutCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.rutcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RutCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rutcloud/RutCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rutcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
