41f196920c49870d676be7cb35759e2135295003
kottzi <denis_levshenko@mail.ru>
2024-04-16 15:30:31 +0300

    update all htmls

--- src/main/resources/static/js/get.js
+++ src/main/resources/static/js/get.js
@@ -1,7 +1,3 @@
 function downloadFile() {
     alert("Функциональность скачивания файлов будет добавлена здесь.");
 }
-
-function returnToMain() {
-    window.location.href = "/";
-}
---
+++ src/main/resources/static/js/return.js
@@ -0,0 +1,3 @@
+function returnToMain() {
+    window.location.href = "/";
+}
